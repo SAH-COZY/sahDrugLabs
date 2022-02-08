d00d89769753b9c81ed5903db48c4c869c20369b
SAHCOZY <sahcozyy@gmail.com>
2022-02-08 14:46:51 +0100

    Added notification event

--- client/main/events.lua
+++ client/main/events.lua
@@ -2,3 +2,8 @@ RegisterNetEvent("sahDrugLabs:ReturnMyLabs")
 AddEventHandler("sahDrugLabs:ReturnMyLabs", function(labs_list)
     MyDrugLabs = labs_list
 end)
+
+RegisterNetEvent("sahDrugLabs:sendNotificationToPlayer")
+AddEventHandler("sahDrugLabs:sendNotificationToPlayer", function(msg)
+    DisplayNotification(msg)
+end)
