45d04671f86d6bee93aa6aedd94eb6ffeb268cd6
SAHCOZY <sahcozyy@gmail.com>
2022-02-08 14:49:44 +0100

    Loading co owners list of every lab at the start of the server

--- server/main/main.lua
+++ server/main/main.lua
@@ -5,7 +5,7 @@ Citizen.CreateThread(function()
     MySQL.ready(function()
         MySQL.Async.fetchAll("SELECT * FROM druglabs_list", {}, function(result)
             for k,v in pairs(result) do
-                AllDrugLabs[tostring(v.LabId)] = InitDrugLab(v.LabId, v.Owner, v.BusinessType, v.ProductionState, v.PercentOfStock, v.PercentOfSupplies, tobool(v.PlyCanResuply), tobool(v.PreparationMade), tobool(v.ResupplyButtonState), json.decode(v.Stats), json.decode(v.Upgrades), json.decode(v.Entry), tobool(v.Bought))
+                AllDrugLabs[tostring(v.LabId)] = InitDrugLab(v.LabId, v.Owner, json.decode(v.Co_Owners), v.BusinessType, v.ProductionState, v.PercentOfStock, v.PercentOfSupplies, tobool(v.PlyCanResuply), tobool(v.PreparationMade), tobool(v.ResupplyButtonState), json.decode(v.Stats), json.decode(v.Upgrades), json.decode(v.Entry), tobool(v.Bought))
             end
             AllDrugLabsLoaded = true
         end)
